a2d4f4bf4b4e3b3459a4dd4b6be59cf7476c2a8b
siquel <squual@kahvipaussi.net>
2013-11-03 23:18:50 +0200

    Lisätty PowerUp komponentti työkalulle ja hookattu se playerille.

---
+++ Farmi/Entities/Components/PowerUpComponent.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Farmi.Entities.Items;
+using Khv.Game.GameObjects;
+using Microsoft.Xna.Framework;
+
+namespace Farmi.Entities.Components
+{
+    class PowerUpComponent : IObjectComponent
+    {
+        public float MinPower { get; private set; }
+        public float MaxPower { get; private set; }
+        public float CurrentPower { get; private set; }
+        public bool Enabled { get; private set; }
+        public float PowerStep { get; private set; }
+
+
+        
+
+        private readonly Tool tool;
+
+        public PowerUpComponent(Tool tool, float minPower, float maxPower)
+        {
+            MinPower = minPower;
+            MaxPower = maxPower;
+            this.tool = tool;
+            PowerStep = 1f;
+            Reset();
+        }
+
+        public void Reset()
+        {
+            CurrentPower = 0f;
+            Enabled = false;
+        }
+
+        public void Enable()
+        {
+            Enabled = true;
+        }
+
+        public void Update(GameTime gametime)
+        {
+            if (!Enabled)
+                return;
+            if (CurrentPower < MaxPower)
+                CurrentPower += PowerStep;
+            else
+                Enabled = false;
+        }
+
+        public bool IsMinimiumMet
+        {
+            get
+            {
+                return CurrentPower >= MinPower;
+            }
+        }
+
+        public bool IsMaximumMet
+        {
+            get
+            {
+                return CurrentPower >= MaxPower;
+            }
+        }
+    }
+}
